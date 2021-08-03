938a508698d1c3c9960ce767137c4e70a7ea07c2
Croix <croix.suhy@gmail.com>
2021-08-03 02:05:54 -0400

    Initial commit

---
+++ infinite monkey theorem.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+"""
+Infinite Monkey Theorem
+By Croix Suhy
+
+Infinite monkey theorem states that if a monkey presses random buttons infinitely on a keyboard,
+it will eventually get to the end result that the user wants.
+"""
+
+# String library for convenience
+letters = string.ascii_letters + " "
+
+
