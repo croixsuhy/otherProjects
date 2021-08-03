2cd194a8085d94c435a5fb96aad3373a1836f897
Croix <croix.suhy@gmail.com>
2021-08-03 02:48:14 -0400

    Created infinite monkey theorem.py

--- infinite monkey theorem.py
+++ infinite monkey theorem.py
@@ -1,15 +1,56 @@
-import string
-import random
+from string import ascii_lowercase
+from random import choice
 
 """
 Infinite Monkey Theorem
 By Croix Suhy
 
 Infinite monkey theorem states that if a monkey presses random buttons infinitely on a keyboard,
-it will eventually get to the end result that the user wants.
+it will eventually get to the end result.
 """
 
-# String library for convenience
-letters = string.ascii_letters + " "
+
+########################################################################
+# Insert word/sentence below (note: you can only letters and spaces)
+# Note the longer the string is, it will take longer
+string = "bro"
+
+# Turn this variable true to see what's going on in the background
+UNDER_THE_HOOD = False
+########################################################################
 
 
+STRING = string.lower()
+
+# ASCII library for convenience
+# Generates characters
+CHARS = ascii_lowercase + " "
+
+# Random string, will be completely randomized
+randomString = ""
+
+
+def random():
+    global randomString
+
+    # Randomly adds characters to string that are the same size as the user imputed string
+    for i in range(len(STRING)):
+        randomString += choice(CHARS)
+
+
+while True:
+    try:
+        # While the string isn't equal to the user imputed string,
+        # it will clear the variable and run the randomizer again
+        if randomString != STRING:
+            if UNDER_THE_HOOD:
+                print(randomString)
+            randomString = ""
+            random()
+        else:
+            # When its completed, the loop will end
+            print(f"Completed: {randomString}")
+            break
+
+    except RuntimeError:
+        print("Can not randomize")
